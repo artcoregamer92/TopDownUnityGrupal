27e5365c8120bdfa245418e2fba7ebb8f4e39aee
LYNDA EVELYN (SCATHLYN) <129218205+artcoregamer92@users.noreply.github.com>
2025-06-11 21:34:43 -0500

    Movimiento Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float inputH;
+    private float inputV;
+    private bool moviendo;
+    private Vector3 puntoDestino;
+    private Vector3 ultimoInput;
+    private Vector3 puntoInteraccion;
+    private Collider2D colliderDelante; //indica el collider que tenemos por delante.
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float radioInteraccion;
+    [SerializeField] private LayerMask queEsColisionable;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inputV == 0)
+        {
+            inputH = Input.GetAxisRaw("Horizontal");
+        }
+
+        if(inputH == 0)
+        {
+            inputV = Input.GetAxisRaw("Vertical");
+        }
+        
+        //Ejecutar movimiento solo si estoy en una casilla y solo si hay input
+        if(!moviendo && (inputH!= 0 || inputV != 0))
+        {
+            //actualizar cual fue mi ultimo input, cual va a ser mi puntoDestino y cual es mi puntoInteraccion.
+            ultimoInput = new Vector3(inputH, inputV, 0);
+            puntoDestino = transform.position + ultimoInput;
+            puntoInteraccion = puntoDestino;
+
+            colliderDelante = LanzarCheck();
+
+            if (!colliderDelante)
+            {
+                StartCoroutine(Mover());
+            }            
+        }             
+    }
+
+    IEnumerator Mover()
+    {
+        moviendo = true;
+        while (transform.position != puntoDestino)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, puntoDestino, velocidadMovimiento * Time.deltaTime);
+            yield return null;
+        }
+
+        //Ante un nuevo destino, necesito refrescar de nuevo puntoInteraccion.
+        puntoInteraccion = transform.position + ultimoInput;
+        moviendo = false;
+    }
+
+    private Collider2D LanzarCheck()
+    {
+        return Physics2D.OverlapCircle(puntoInteraccion, radioInteraccion, queEsColisionable);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(puntoInteraccion, radioInteraccion);
+    }
+}
